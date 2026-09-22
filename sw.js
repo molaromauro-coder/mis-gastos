9eac1604b4569f0afb4568ef5c4576b206a02602
molaromauro-coder <molaro.mauro@gmail.com>
2026-09-22 00:50:53 -0300

    Finalize main branch conflict resolution

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const CACHE='mis-gastos-v2',FILES=['./','./index.html','./styles.css','./app.js','./parser.js','./manifest.webmanifest','./icons/icon.svg'];
+// Change this version whenever merged application assets change so installed PWAs
+// do not keep serving files from a previous branch.
+const CACHE='mis-gastos-v3',FILES=['./','./index.html','./styles.css','./app.js','./parser.js','./manifest.webmanifest','./icons/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
